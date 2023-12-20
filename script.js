fa04ba74c1a859dca833317d324ef90a275b110b
vuckoviic <vuckovica64@gmail.com>
2023-12-20 20:44:51 +0100

    Create Gameboard factory function
    which returns getBoard, drawToken and printBoard methods

---
+++ script.js
@@ -0,0 +1,28 @@
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    // creating 2d array
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => {
+        return board;
+    }
+    
+    const drawToken = function (row, column, player) {
+        // logic fow drawing X or O in table
+    }
+    
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+    };
+
+    return { getBoard, drawToken, printBoard };
+}
