d3e14b1ae4f4cf614b2f4a3941a90baa67e75bd4
vuckoviic <vuckovica64@gmail.com>
2023-12-21 16:39:16 +0100

    Create Gameboard factory function

---
+++ script.js
@@ -0,0 +1,19 @@
+Gameboard = function() {
+    let board = [];
+
+    drawToken = function(row, column, player) {
+        // method takes row, column and player. 
+        //From player object it finds player's token
+        //and calculates positions relying on row and column
+    }
+
+    getBoard = function() {
+        return board;
+    }
+
+    return {
+        drawToken,
+        getBoard
+    }
+}
+
